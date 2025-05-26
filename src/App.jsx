ce289fca0910ac7e23ab9cb786993c11066b9940
Moinul6039 <dhakadesign2000@gmail.com>
2025-05-26 12:34:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { ZIM } from 'zego-zim-web';
+
+function App() {
+  const [zimInstance,setZimInstance] = useState(null)
+  const [userInfo,setUserInfo] = useState(null)
+  const [messageText,setMessageText] = useState("")
+  const [message,setMessage] = useState([])
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
